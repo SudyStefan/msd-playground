f6d2463a93a03af63d95df7197de3f8172a6ab6a
Stefan Sudy <stefan.sudy@edu.fh-joanneum.at>
2026-03-05 14:20:28 +0100

    after english

--- python_playground/selection.py
+++ python_playground/selection.py
@@ -1,12 +1,11 @@
 def sort(list_to_sort: list[int]) -> tuple[list[int], int]:
     loopcount = 0
-    for i in range(0, len(list_to_sort)):
-        key = list_to_sort[i]
+    for i in range(0, len(list_to_sort) - 1):
+        selected_item = list_to_sort[i]
         j = i + 1
-        while key > list_to_sort[j] and j < len(list_to_sort):
+        while list_to_sort[j] > selected_item and j < len(list_to_sort) - 1:
             j += 1
             loopcount += 1
-        list_to_sort[j - 1] = key
         list_to_sort[i] = list_to_sort[j]
-
+        list_to_sort[j] = selected_item
     return list_to_sort, loopcount
