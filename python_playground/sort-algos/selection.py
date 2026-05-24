08549c1bd219e922302a54f55c6f5095b74d961d
BergMichel <stefan.sudy1@gmail.com>
2026-05-24 10:17:47 +0200

    clean repo

--- python_playground/sort-algos/selection.py
+++
@@ -1,9 +0,0 @@
-def sort(list_to_sort: list[int]) -> list[int]:
-    for i in range(0, len(list_to_sort) - 1):
-        min_index = i
-        for k in range(i + 1, len(list_to_sort)):
-            if list_to_sort[k] < list_to_sort[min_index]:
-                min_index = k
-        if min_index != i:
-            list_to_sort[i], list_to_sort[min_index] = list_to_sort[min_index], list_to_sort[i]
-    return list_to_sort
